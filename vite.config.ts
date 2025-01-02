6688e6afc0fdc316e8320a95fc34a9e569772a85
zsm <782683353@qq.com>
2025-01-02 16:36:46 +0800

    fix: 更新缓存配置，解决 304 状态问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// 生成构建时间戳
+const timestamp = new Date().getTime();
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -11,6 +14,10 @@ export default defineConfig({
     sourcemap: true,
     rollupOptions: {
       output: {
+        // 添加时间戳到文件名
+        entryFileNames: `[name].${timestamp}.js`,
+        chunkFileNames: `[name].${timestamp}.js`,
+        assetFileNames: `[name].${timestamp}.[ext]`,
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
         }
